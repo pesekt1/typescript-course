8862db4430b15932121b49332a20a2596cdf3a82
Tomas Pesek <pesekt@gmail.com>
2022-05-30 17:49:10 +0200

    lodash

--- src/javascript.ts
+++ src/javascript.ts
@@ -1,9 +1,12 @@
 import { calculateTax } from "./tax";
 import { calculateTax as calculateTax2 } from "./tax2";
+import { calculateTax as calculateTax3 } from "./tax3";
 
 //problem: now argument is type of any
 //undefined is used when no argument is provided
 let tax = calculateTax(); //compiler does not see the problem
 console.log(tax);
 
-//let tax2 = calculateTax2(); //the compiler can spot the error
+//let tax2 = calculateTax2(); //the compiler can spot the error because of JSDoc
+
+//let tax3 = calculateTax3(); //the compiler can spot the error because of tax3.d.ts declaration
---
+++ src/tax3.js
@@ -0,0 +1,4 @@
+//@ts-nocheck
+export function calculateTax(income) {
+  return income * 0.3;
+}
