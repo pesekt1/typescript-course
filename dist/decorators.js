6cc2a6b47d91baf0fda42358434e16248627d0de
Tomas Pesek <pesekt@gmail.com>
2022-05-30 15:42:30 +0200

    finished decorators

--- dist/decorators.js
+++ dist/decorators.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Component(constructor) {
     console.log("component decorator called");
     constructor.prototype.uniquieId = Date.now();
@@ -28,4 +31,99 @@ let ProfileComponent2 = class ProfileComponent2 {
 ProfileComponent2 = __decorate([
     Component2({ selector: "#app-profile" })
 ], ProfileComponent2);
+function Pipe(constructor) {
+    console.log("pipe decorator called");
+    constructor.prototype.pipe = true;
+}
+let ProfileComponent3 = class ProfileComponent3 {
+};
+ProfileComponent3 = __decorate([
+    Component2({ selector: "#app-profile" }),
+    Pipe
+], ProfileComponent3);
+function Log(target, methodName, descriptor) {
+    const original = descriptor.value;
+    descriptor.value = function (message) {
+        console.log("Before");
+        original.call(this, message);
+        console.log("After");
+    };
+}
+class Messenger {
+    say(message) {
+        console.log("Messenger says " + message);
+    }
+}
+__decorate([
+    Log
+], Messenger.prototype, "say", null);
+let messenger = new Messenger();
+messenger.say("hello!");
+function Log2(target, methodName, descriptor) {
+    const original = descriptor.value;
+    descriptor.value = function (...args) {
+        console.log("Before");
+        original.call(this, ...args);
+        console.log("After");
+    };
+}
+function Capitalize(target, methodName, descriptor) {
+    const original = descriptor.get;
+    descriptor.get = function () {
+        const result = original === null || original === void 0 ? void 0 : original.call(this);
+        return typeof result === "string" ? result.toUpperCase() : result;
+    };
+}
+class Buyer {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    get fullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+__decorate([
+    Capitalize
+], Buyer.prototype, "fullName", null);
+let buyer = new Buyer("Alex", "Smith");
+console.log(buyer.fullName);
+function MinLength(length) {
+    return (target, propertyName) => {
+        let value;
+        const descriptor = {
+            get() {
+                return value;
+            },
+            set(newValue) {
+                if (newValue.length < length)
+                    throw new Error(`${propertyName} should be at least ${length} characters long`);
+                value = newValue;
+                return newValue;
+            },
+        };
+        Object.defineProperty(target, propertyName, descriptor);
+    };
+}
+class User {
+    constructor(password) {
+        this.password = password;
+    }
+}
+__decorate([
+    MinLength(4)
+], User.prototype, "password", void 0);
+let user2 = new User("1234");
+console.log(user2.password);
+const watchedParameters = [];
+function Watch(target, propertyName, parameterIndex) {
+    watchedParameters.push({ methodName: propertyName, parameterIndex });
+}
+class Vehicle {
+    move(distance, speed) { }
+}
+__decorate([
+    __param(1, Watch)
+], Vehicle.prototype, "move", null);
+console.log(watchedParameters);
 //# sourceMappingURL=decorators.js.map
--- src/decorators.ts
+++ src/decorators.ts
@@ -22,3 +22,120 @@ function Component2(options: ComponentOptions) {
 
 @Component2({ selector: "#app-profile" })
 class ProfileComponent2 {}
+
+//Decorator composition --------------------------------
+function Pipe(constructor: Function) {
+  console.log("pipe decorator called");
+  constructor.prototype.pipe = true;
+}
+
+@Component2({ selector: "#app-profile" })
+@Pipe
+class ProfileComponent3 {}
+
+//method decorators ------------------------
+//descriptor.value - stores the function, we can even replace it with new implementation
+//demporarily deactivate noUnusedParameters in tsconfig
+function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const original = descriptor.value as Function;
+  descriptor.value = function (message: string) {
+    console.log("Before");
+    original.call(this, message);
+    console.log("After");
+  };
+}
+
+class Messenger {
+  @Log
+  say(message: string) {
+    console.log("Messenger says " + message);
+  }
+}
+
+let messenger = new Messenger();
+messenger.say("hello!");
+
+//flexible solution for any amount of arguments of any type:
+function Log2(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const original = descriptor.value as Function;
+  descriptor.value = function (...args: any) {
+    console.log("Before");
+    original.call(this, ...args);
+    console.log("After");
+  };
+}
+
+//accessor decorators------------------------
+function Capitalize(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const original = descriptor.get;
+  descriptor.get = function () {
+    const result = original?.call(this);
+    return typeof result === "string" ? result.toUpperCase() : result;
+  };
+}
+
+class Buyer {
+  constructor(public firstName: string, public lastName: string) {}
+
+  @Capitalize
+  get fullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+let buyer = new Buyer("Alex", "Smith");
+console.log(buyer.fullName);
+
+//property decorators------------------------
+function MinLength(length: number) {
+  return (target: any, propertyName: string) => {
+    let value: string;
+    const descriptor: PropertyDescriptor = {
+      get() {
+        return value;
+      },
+      set(newValue: string) {
+        if (newValue.length < length)
+          throw new Error(
+            `${propertyName} should be at least ${length} characters long`
+          );
+        value = newValue;
+        return newValue;
+      },
+    };
+    Object.defineProperty(target, propertyName, descriptor);
+  };
+}
+
+class User {
+  @MinLength(4)
+  password: string;
+  constructor(password: string) {
+    this.password = password;
+  }
+}
+
+let user2 = new User("1234");
+console.log(user2.password);
+
+//Parameter decorators------------------------
+type WatchedParameter = {
+  methodName: string;
+  parameterIndex: number;
+};
+
+const watchedParameters: WatchedParameter[] = [];
+
+function Watch(target: any, propertyName: string, parameterIndex: number) {
+  watchedParameters.push({ methodName: propertyName, parameterIndex });
+}
+
+class Vehicle {
+  move(distance: number, @Watch speed: number) {}
+}
+
+console.log(watchedParameters);
